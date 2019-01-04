78436fc7b6d1f8702f9b980ebcb7702dadc7c824
Abhijeet Singh <abhijeetsinghraj2345@gmail.com>
2019-01-04 18:42:45 +0530

    Handling PermissionError with try-except

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from classes import Store_data,root
 
 
 def main():
-
-    wb = load_workbook('excel.xlsx')
-    sheet = wb.active
-    obj1 = Store_data(sheet)
-    obj1.layout()
-    root.mainloop()
-    obj1.excel()
-    obj1.insert()
-    wb.save('excel.xlsx')
+    try:
+        wb = load_workbook('excel.xlsx')
+        sheet = wb.active
+        obj1 = Store_data(sheet)
+        obj1.layout()
+        root.mainloop()
+        obj1.excel()
+        obj1.insert()
+        wb.save('excel.xlsx')
+    except PermissionError:
+        print('Permission denied : excel.xlsx file is already opened for other use')
